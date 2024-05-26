b65cad0857a6c1149b15db32ffa74e1f715a290f
ZeroTjeen <jentedefeyter@gmail.com>
2024-05-26 19:11:21 +0200

    fixed the code so it works properly

--- modules/ledring.h
+++ modules/ledring.h
@@ -3,13 +3,12 @@
 #define DATA_PIN A0
 #define SCORE_PIN 6
 #define SCORE_LEDS 6
-#define BRIGHTNESS 75
+#define BRIGHTNESS 25
 CRGB leds[NUM_LEDS];
-CRGB sleds[NUM_LEDS];
+CRGB sleds[SCORE_LEDS];
 
 bool reachedEnd = false;
 byte gameState = 0;
-//byte ledSpeed = 0;
 int period = 1000;
 unsigned long time_now = 0;
 byte Position = 0;
@@ -17,27 +16,26 @@ byte level = 0;
 
 const byte ledSpeed[6] = {50, 40, 30, 20, 14, 7};
 
-//Debounce
-bool findRandom = false;
-byte spot = 0;
-
-
+// Static spots for each level
+const byte staticSpots[6] = {3, 5, 7, 9, 11, 13};
 
 void clearLEDS() {
   for (byte i = 0; i < NUM_LEDS; i++) {
-    leds[i].setRGB(0, 0, 0);
+    leds[i] = CRGB::Black;
   }
+  FastLED.show();
 }
 
 void PlayGame(byte bound1, byte bound2) {
-  leds[Position].setRGB(255, 0, 0);
-  if (Position < bound1 + 1 || Position > bound2 + 1) {
-    leds[Position - 1].setRGB(0, 0, 0);
+  if (Position > 0) {
+    leds[Position - 1] = CRGB::Black;
+  } else {
+    leds[NUM_LEDS - 1] = CRGB::Black;
   }
+  leds[Position] = CRGB::Red;
   FastLED.show();
   Position++;
   if (Position >= NUM_LEDS) {
-    leds[Position - 1].setRGB(0, 0, 0);
     Position = 0;
   }
 }
@@ -45,19 +43,15 @@ void PlayGame(byte bound1, byte bound2) {
 void winner() {
   for (byte i = 0; i < 3; i++) {
     for (byte j = 0; j < NUM_LEDS; j++) {
-      leds[j].setRGB(0, 255, 0);
-        tone(9, 1000, 250);
+      leds[j] = CRGB::Green;
+      tone(9, 1000, 250);
     }
     FastLED.show();
     delay(500);
     clearLEDS();
-    FastLED.show();
     delay(500);
-  
   }
-  findRandom = true;
   Position = 0;
-
   gameState = level + 1;
   if (gameState > 6) {
     gameState = 0;
@@ -67,50 +61,45 @@ void winner() {
 void loser() {
   for (byte i = 0; i < 3; i++) {
     for (byte j = 0; j < NUM_LEDS; j++) {
-      leds[j].setRGB(255, 0, 0);
+      leds[j] = CRGB::Red;
       tone(9, 200, 250);
     }
     FastLED.show();
     delay(500);
     clearLEDS();
-    FastLED.show();
     delay(500);
   }
   gameState = 0;
 }
 
-
-void winAll(){
-  
-}
-
 void ledringSetup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, SCORE_PIN, GRB>(sleds, SCORE_LEDS);
-  pinMode(A3, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
   Serial.begin(9600);
   Serial.println("Reset");
 }
 
 void ledringLoop() {
-  FastLED.setBrightness(BRIGHTNESS );
-  if (gameState == 0) {
-    fill_rainbow(leds, NUM_LEDS, 0, 20); //2 = longer gradient strip
-    fill_rainbow(sleds, SCORE_LEDS, 0, 40); //2 = longer gradient strip
+  FastLED.setBrightness(BRIGHTNESS);
 
-    if (digitalRead(A3) == LOW) {
+  // Level setup
+  if (gameState == 0) {
+    fill_rainbow(leds, NUM_LEDS, 0, 20);
+    fill_rainbow(sleds, SCORE_LEDS, 0, 40);
+
+    if (digitalRead(2) == LOW) {
       Position = 0;
-      findRandom = true;
       delay(500);
       for (byte i = 0; i < NUM_LEDS; i++) {
-        leds[i].setRGB(0, 0, 0);
+        leds[i] = CRGB::Black;
         delay(40);
         FastLED.show();
-       int thisPitch = map (i, 60, 0, 100, 1500);
-       tone(9, thisPitch,120);
+        int thisPitch = map(i, 60, 0, 100, 1500);
+        tone(9, thisPitch, 120);
       }
       for (byte i = 0; i < SCORE_LEDS; i++) {
-        sleds[i].setRGB(0, 0, 0);
+        sleds[i] = CRGB::Black;
         delay(100);
         FastLED.show();
       }
@@ -118,93 +107,23 @@ void ledringLoop() {
     }
     FastLED.show();
   }
+
+  byte spot = staticSpots[gameState - 1];  // Define spot based on gameState
+
+  // Level 1
   if (gameState == 1) {
     period = ledSpeed[0];
     if (millis() > time_now + period) {
       time_now = millis();
-      if (findRandom) {
-        spot = random(56) + 3;
-        findRandom = false;
-      }
-      leds[spot - 1].setRGB(255, 140, 0);
-      leds[spot].setRGB(0, 255, 0);
-      leds[spot + 1].setRGB(255, 110, 0);
-      sleds[0].setRGB(0, 255, 0);
-      PlayGame(spot - 1, spot + 1);
+      leds[spot - 1] = CRGB(255, 140, 0);
+      leds[spot] = CRGB(0, 255, 0);
+      leds[spot + 1] = CRGB(255, 110, 0);
+      sleds[0] = CRGB(0, 255, 0);
+      PlayGame(spot + 1, spot + 2);
     }
-    if (digitalRead(A3) == LOW) {
+    if (digitalRead(2) == LOW) {
       delay(300);
-      findRandom = false;
-      if (Position > spot - 1 && Position < spot + 3) {
-        level = gameState;
-        gameState = 98;
-      } else {
-        gameState = 99;
-      }
-    }
-  }
-  if (gameState == 2) {
-  //    period = 320;
-    period = ledSpeed[1];
-    if (millis() > time_now + period) {
-      time_now = millis();
-      if (findRandom) {
-        spot = random(56) + 3;
-        findRandom = false;
-      }
-      leds[spot - 1].setRGB(255, 190, 0);
-      leds[spot].setRGB(0, 255, 0);
-      leds[spot + 1].setRGB(255, 190, 0);
-      sleds[1].setRGB(255, 255, 0);
-      PlayGame(spot - 1, spot + 1);
-    }
-    if (digitalRead(A3) == LOW) {
-      delay(300);
-      if (spot - 1 && Position < spot + 3) {
-        level = gameState;
-        gameState = 98;
-      } else {
-        gameState = 99;
-      }
-    }
-  }
-  if (gameState == 3) {
-    period = ledSpeed[2];
-    if (millis() > time_now + period) {
-      time_now = millis();
-      if (findRandom) {
-        spot = random(56) + 3;
-        findRandom = false;
-      }
-      leds[spot].setRGB(0, 255, 0);
-      sleds[2].setRGB(255, 50, 0);
-      PlayGame(spot, spot);
-    }
-    if (digitalRead(A3) == LOW) {
-      delay(300);
-      if (Position == spot+1) {
-        level = gameState;
-        gameState = 98;
-      } else {
-        gameState = 99;
-      }
-    }
-  }
-  if (gameState == 4) {
-    period = ledSpeed[3];
-    if (millis() > time_now + period) {
-      time_now = millis();
-      if (findRandom) {
-        spot = random(56) + 3;
-        findRandom = false;
-      }
-      leds[spot].setRGB(0, 255, 0);
-      sleds[3].setRGB(255, 0, 0);
-      PlayGame(spot, spot);
-    }
-    if (digitalRead(A3) == LOW) {
-      delay(300);
-      if (Position == spot+1) {
+      if (Position == spot + 2 || Position == spot || Position == spot + 1) {
         level = gameState;
         gameState = 98;
       } else {
@@ -213,21 +132,80 @@ void ledringLoop() {
     }
   }
 
+  // Level 2
+  if (gameState == 2) {
+    period = ledSpeed[0];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      leds[spot - 1] = CRGB(255, 190, 0);
+      leds[spot] = CRGB(0, 255, 0);
+      leds[spot + 1] = CRGB(255, 190, 0);
+      sleds[1] = CRGB(255, 255, 0);
+      PlayGame(spot + 1, spot + 2);
+    }
+    if (digitalRead(2) == LOW) {
+      delay(300);
+      if (Position == spot + 2 || Position == spot || Position == spot + 1) {
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+
+  // Level 3
+  if (gameState == 3) {
+    period = ledSpeed[0];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      leds[spot] = CRGB(0, 255, 0);
+      sleds[2] = CRGB(255, 50, 0);
+      PlayGame(spot + 1, spot + 1);
+    }
+    if (digitalRead(2) == LOW) {
+      delay(300);
+      if (Position == spot + 1) {
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+
+  // Level 4
+  if (gameState == 4) {
+    period = ledSpeed[0];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      leds[spot] = CRGB(0, 255, 0);
+      sleds[3] = CRGB(255, 0, 0);
+      PlayGame(spot + 1, spot + 1);
+    }
+    if (digitalRead(2) == LOW) {
+      delay(300);
+      if (Position == spot + 1) {
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+
+  // Level 5
   if (gameState == 5) {
     period = ledSpeed[4];
     if (millis() > time_now + period) {
       time_now = millis();
-      if (findRandom) {
-        spot = random(56) + 3;
-        findRandom = false;
-      }
-      leds[spot].setRGB(0, 255, 0);
-      sleds[4].setRGB(0, 50, 255);
-      PlayGame(spot , spot);
+      leds[spot] = CRGB(0, 255, 0);
+      sleds[4] = CRGB(0, 50, 255);
+      PlayGame(spot + 1, spot + 1);
     }
-    if (digitalRead(A3) == LOW) {
+    if (digitalRead(2) == LOW) {
       delay(300);
-      if (Position == spot+1) {
+      if (Position == spot + 1) {
         level = gameState;
         gameState = 98;
       } else {
@@ -236,21 +214,18 @@ void ledringLoop() {
     }
   }
 
+  // Level 6
   if (gameState == 6) {
     period = ledSpeed[5];
     if (millis() > time_now + period) {
       time_now = millis();
-      if (findRandom) {
-        spot = random(56) + 3;
-        findRandom = false;
-      }
-      leds[spot].setRGB(0, 255, 0);
-      sleds[5].setRGB(0, 150, 255);
-      PlayGame(spot , spot);
+      leds[spot] = CRGB(0, 255, 0);
+      sleds[5] = CRGB(0, 150, 255);
+      PlayGame(spot + 1, spot + 1);
     }
-    if (digitalRead(A3) == LOW) {
+    if (digitalRead(2) == LOW) {
       delay(300);
-      if (Position == spot+1) {
+      if (Position == spot + 1) {
         level = gameState;
         gameState = 98;
       } else {
@@ -258,7 +233,7 @@ void ledringLoop() {
       }
     }
   }
-  
+
   if (gameState == 98) {
     winner();
   }
@@ -266,4 +241,3 @@ void ledringLoop() {
     loser();
   }
 }
-
