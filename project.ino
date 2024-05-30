6ce89c94adae148a3f043415a50ef13534337323
ZeroTjeen <jentedefeyter@gmail.com>
2024-05-30 19:40:23 +0200

    added example files

--- project.ino
+++ project.ino
@@ -1,12 +1,13 @@
 #include "modules/ledring.h"
 #include "modules/lcd.h"
+#include "modules/mp3_player.h"
 
 void setup()
 {
+  setupmp3();
 }
 
 void loop()
 {
-  ledring_init();
-  lcd_init();
+  loopmp3();
 }
